2eef54de6e05580a7aaae29e2a1978bd070c09f5
Canbers <canby.ryan@gmail.com>
2023-03-25 16:20:14 -0700

    syncing from PC

--- public/app.js
+++ public/app.js
@@ -1,45 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import { updateDoc, doc } from "firebase/firestore";
+import openai from "openai";
 
+// Set the OpenAI API key
+openai.apiKey = process.env.OPENAI_API_KEY;
+
+// Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyD171rxtQ4U-qOJIn0rCvbotMxtlkFCPkg",
-    authDomain: "gpt-dragons.firebaseapp.com",
-    databaseURL: "https://gpt-dragons-default-rtdb.firebaseio.com",
-    projectId: "gpt-dragons",
-    storageBucket: "gpt-dragons.appspot.com",
-    messagingSenderId: "439621424805",
-    appId: "1:439621424805:web:07adba9fb895e86f22df4c",
-    measurementId: "G-8GQ89VJS29"
-  };
-  
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function createCharacter(gameId, characterType, currentGameState = null) {
-    // Your character creation logic here
-  }
-
-
-async function generateNpc(currentGameState, races, classes) {
-    // Your GPT-3 powered NPC generation logic here
-  }
-  
-
-async function saveCharacter(gameId, playerName, characterName, characterRace, characterClass, attributes, characterType) {
-    const characterRef = doc(db, "games", gameId, "characters");
-    const characterData = {
-      playerName,
-      characterName,
-      characterRace,
-      characterClass,
-      attributes,
-      characterType,
-    };
-  
-    const docRef = await addDoc(characterRef, characterData);
-    return docRef.id;
-  }
-  
 
 const inputField = document.getElementById('input');
 const submitBtn = document.getElementById('submit-btn');
@@ -53,7 +38,7 @@ inputField.addEventListener('keydown', (event) => {
 
 submitBtn.addEventListener('click', submitAction);
 
-function submitAction() {
+export function submitAction() {
     const inputText = inputField.value.trim();
   
     if (inputText) {
@@ -93,3 +78,74 @@ function submitAction() {
     }
   }
   
+  
+
+
+
+
+  async function startNewGame(gameData) {
+    try {
+      const gameRef = await addDoc(collection(db, "Games"), gameData);
+      console.log(`New game created with ID: ${gameRef.id}`);
+      return gameRef.id;
+    } catch (e) {
+      console.error("Error adding game document: ", e);
+    }
+  }
+
+  import { updateDoc, doc } from "firebase/firestore";
+
+async function startGameWithStoryline() {
+  // Generate storyline options
+  const storylines = await generateStorylineOptions();
+
+  // Display storyline options and get user's selection
+  // Replace this with the actual user interface for selecting a storyline
+  const selectedStoryline = storylines[0]; // Assume the user selected the first storyline
+
+  // Start a new game and save it to the database
+  const gameData = {
+    storyline: selectedStoryline,
+    createdAt: new Date(),
+  };
+
+  const gameId = await startNewGame(gameData);
+
+  // Save the game seed (storyline) to the game record in the database
+  try {
+    const gameRef = doc(db, "Games", gameId);
+    await updateDoc(gameRef, {
+      seed: selectedStoryline,
+    });
+  } catch (e) {
+    console.error("Error updating game seed: ", e);
+  }
+}
+
+
+async function generateStorylineOptions() {
+  try {
+    const systemMessage = "You are in the position of game master for a Dungeons and Dragons style game. The game is about to start and you want to give your players a choice of what kind of story they want to play. Generate creative, original, and unique Dungeons & Dragons scenarios that provide an intriguing hook and are enticing to players. Do not add 'scenario' or a number as a prefix.";
+    const prompt = "Generate 3 scenarios for a Dungeons & Dragons game.";
+
+    const response = await openai.Completion.create({
+      engine: "text-davinci-002",
+      prompt: `${systemMessage}\n\nUser: ${prompt}`,
+      max_tokens: 350,
+      n: 1,
+      stop: null,
+      temperature: 0.9,
+    });
+
+    const generatedText = response.choices[0].text;
+    const scenarios = generatedText
+      .split("\n")
+      .filter((s) => s.trim())
+      .map((s) => s.trim());
+
+    return scenarios;
+  } catch (error) {
+    console.error(`An error occurred: ${error}`);
+    throw error;
+  }
+}
